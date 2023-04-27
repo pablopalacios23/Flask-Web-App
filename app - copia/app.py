f187c41291e0dc67bedc10d4a8fe0df67fe499a5
Pablo Palacios López <115399425+pablopalacios23@users.noreply.github.com>
2023-04-27 12:29:03 +0200

    Commit 3
    Las respuestas se generan en un documento Word

--- app - copia/app.py
+++ app - copia/app.py
@@ -2,6 +2,10 @@ import os
 import json
 from flask import Flask, jsonify, render_template, request
 
+from docx import Document
+from docx.shared import Inches
+from dotenv import load_dotenv
+
 from dotenv import load_dotenv
 
 load_dotenv('./.flaskenv')
@@ -22,8 +26,14 @@ def index():
             respuesta = request.form[pregunta['nombre']]
             pregunta['respuesta'] = respuesta
             respuestas[pregunta['texto']] = respuesta
-        with open('respuestas.json', 'w') as f:
-            json.dump(respuestas, f)
+
+        # Crear el documento Word y agregar las respuestas
+        document = Document()
+        document.add_heading('Respuestas del cuestionario', 0)
+        for pregunta in preguntas:
+            document.add_paragraph(f"{pregunta['texto']}: {pregunta['respuesta']}", style='List Bullet')
+        document.save('respuestas.docx')
+
         return 'Formulario enviado correctamente'
 
     return render_template('index.html', preguntas=preguntas)
