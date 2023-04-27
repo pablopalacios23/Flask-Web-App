c24eed3a35edd0ec1c4483e658585155844ec5bf
Pablo Palacios López <115399425+pablopalacios23@users.noreply.github.com>
2023-04-27 12:17:44 +0200

    Commit 2
    Ya guarda las respuestas correctamente

--- app - copia/app.py
+++ app - copia/app.py
@@ -11,22 +11,22 @@ app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def index():
     preguntas = [
-            {"texto": "¿Cuál es tu nombre?", "respuesta": ""},
-            {"texto": "¿Cuál es tu edad?", "respuesta": ""},
-            {"texto": "¿Cuál es tu género?", "respuesta": ""}
+            {"texto": "¿Cuál es tu nombre?", "nombre": "nombre", "respuesta": ""},
+            {"texto": "¿Cuál es tu edad?", "nombre": "edad", "respuesta": ""},
+            {"texto": "¿Cuál es tu género?", "nombre": "genero", "respuesta": ""}
     ]
 
     if request.method == 'POST':
         respuestas = {}
         for pregunta in preguntas:
-            respuestas[pregunta['texto']] = pregunta['respuesta']
+            respuesta = request.form[pregunta['nombre']]
+            pregunta['respuesta'] = respuesta
+            respuestas[pregunta['texto']] = respuesta
         with open('respuestas.json', 'w') as f:
             json.dump(respuestas, f)
         return 'Formulario enviado correctamente'
 
-
-    return render_template('index.html', preguntas = preguntas)
-
+    return render_template('index.html', preguntas=preguntas)
 
 if __name__ == '__main__':
     app.run()
